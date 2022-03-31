b00a8e71e03876d0733649ed068f1e4930e45a47
JinkerLeong <jinker2003@gmail.com>
2022-03-31 16:57:04 +0800

    first commit

---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+using Bbob.Plugin;
+
+namespace bbob_plugin_mathjax;
+public class Class1 : IPlugin
+{
+    public void GenerateCommand(string filePath, GenerationStage stage)
+    {
+        if (stage != GenerationStage.FinalProcess) return;
+
+        string source = "<script>MathJax.typeset()</script>";
+        PluginHelper.modifyRegisteredObject<dynamic>("article", (ref dynamic? article)=>{
+            if (article != null)
+            {
+                article.contentParsed += source;
+            }
+        });
+    }
+    public Action? CommandComplete(Commands cmd)
+    {
+        if (cmd != Commands.GenerateCommand) return null;
+        string indexFile = Path.Combine(PluginHelper.DistributionDirectory, "index.html");
+        string indexPlain = File.ReadAllText(indexFile);
+        string mathJaxOption = "<script>MathJax={options:{processHtmlClass:'math',ignoreHtmlClass:'.*'}};</script>";
+        string src = "<script type=\"text/javascript\" id=\"MathJax-script\" async src=\"https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js\"></script>";
+        string patternHead = @"<head>(.*)</head>";
+        string replacement1 = $"<head>{src}{mathJaxOption}$1</head>";
+        indexPlain = Regex.Replace(indexPlain, patternHead, replacement1, RegexOptions.Singleline);
+        File.WriteAllText(indexFile, indexPlain);
+        return null;
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# Install
+```
+bbob add -a https://github.com/Reknij/bbob-plugin-mathjax/releases/download/v1.0.0/bbob-plugin-mathjax.rar
+```
+
+# How to use?
+It will auto working.
+
+Write some math syntax to your article then generate to test:
+```
+// articles/testMath.md
+...
+$x^4$ and $y^6$
+```
+
+Result:
+
+$x^4$ and $y^6$
